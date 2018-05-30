e9b4167c8b0ea43843cc1a538d11eb248e0bbd23
behconsci <behconsci@gmail.com>
2018-05-30 23:31:02 +0200

    fixed again

--- project_name/apps/user/admin.py
+++ project_name/apps/user/admin.py
@@ -1,28 +1,10 @@
 from django.contrib import admin
 
-from .models import Profile, UserPayments
+from .models import Profile
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'paid_at', 'api_token', 'idate', 'country')
-    # readonly_fields = ('confirm_hash', 'country', 'user')
-
-    def paid_at(self, obj):
-        if not obj.user.user_payments.last():
-            # no payment done
-            return 'no payment yet'
-
-        user_payment = obj.user.user_payments.latest('paid_at')
-        return user_payment.paid_at.strftime('%d.%m.%Y at %H:%M')
-    paid_at.short_description = 'Payment'
+    list_display = ('id', 'user', 'idate')
 
 
 admin.site.register(Profile, ProfileAdmin)
-
-
-class UserPaymentsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'paid_at', 'amount')
-    # readonly_fields = ('paid_at', 'amount', 'user')
-
-
-admin.site.register(UserPayments, UserPaymentsAdmin)
