ac0e20840640237042569d879939d5baea529c0f
behconsci <behconsci@gmail.com>
2018-05-30 17:55:17 +0200

    added index

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from {{ project_name }}.apps.core.views import letsencrypt
+from {{ project_name }}.apps.core.views import letsencrypt, index
 
 
 urlpatterns = [
+    path('', index, name='index'),
     path('.well-known/acme-challenge/pFmV21J6LJNm4q3E4H0rY6iUDjSjVJmLlSGCnPs3u0Y', letsencrypt),
     path('admin/', admin.site.urls),
 ]
