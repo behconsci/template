48c456adf24a1dae165104961beac619a8c3344e
behconsci <behconsci@gmail.com>
2018-08-02 15:23:52 +0200

    added static and media urls in urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from {{ project_name }}.apps.core.views import letsencrypt, index
 
@@ -11,3 +13,7 @@ urlpatterns = [
     path('user/', include('{{ project_name }}.apps.user.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
